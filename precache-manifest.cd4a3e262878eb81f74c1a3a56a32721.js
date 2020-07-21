fb9a1a0b643abbacfc2f1353a56191b4b238291d
Ritesh-Dabral <riteshdabral1@gmail.com>
2020-07-21 18:52:05 +0530

    Updates

---
+++ precache-manifest.cd4a3e262878eb81f74c1a3a56a32721.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c569eb06b99d50e2d6a56a12440f1381",
+    "url": "/reacttodoapp/index.html"
+  },
+  {
+    "revision": "23f5105a6c88f3a83dac",
+    "url": "/reacttodoapp/static/css/main.a8f73294.chunk.css"
+  },
+  {
+    "revision": "e0e127ba9773f75d24be",
+    "url": "/reacttodoapp/static/js/2.4ee56a9d.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/reacttodoapp/static/js/2.4ee56a9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23f5105a6c88f3a83dac",
+    "url": "/reacttodoapp/static/js/main.9d241c5d.chunk.js"
+  },
+  {
+    "revision": "ab693671f5df9f5e2a18",
+    "url": "/reacttodoapp/static/js/runtime-main.a1dd6a5e.js"
+  }
+]);
